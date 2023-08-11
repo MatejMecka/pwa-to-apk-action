e5b912130dc793210c5a6692f9ca85b6353edfdc
Matej Plavevski <matej.plavevski@gmail.com>
2023-08-11 19:46:44 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@
 echo "========================= Changing directory to $1 ========================="
 cd $1
 echo "========================= Building APK, it may take 3-4 minutes or more ========================="
-( sleep 5 && while [ 1 ]; do sleep 1; echo y; done ) | bubblewrap build --skipPwaValidation --skipSigning
+( sleep 5 && while [ 1 ]; do sleep 1; echo y; done ) | bubblewrap build --skipPwaValidation
 cp ./*.apk ..
+cp ./*.aab ..
 echo "========================= APK building finished ========================="
